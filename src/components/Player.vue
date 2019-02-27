54c734aecf33f33df8bac8ba3e50a25b4056f7d6
xulingyu@100tal.com <xulingyu@100tal.com>
2019-02-27 18:47:09 +0800

    vue

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,13 +1,195 @@
 <template>
-    <div>
-       
+  <div>
+    <!-- 专辑信息 -->
+    <div class="album">
+      <div class="album-mask" :style="{background: 'url('+albumImg+') no-repeat center/cover'}"></div>
+      <div class="album-img">
+        <img :src="albumImg" alt>
+      </div>
+      <div class="album-info">
+        <p class="album-info-title">{{albumTitle}}</p>
+        <p class="album-info-author">{{albumAuthor}}</p>
+        <div class="album-info-control clearfix">
+          <div class="album-info-control-icon">
+            <i class="icon iconfont icon-shangyishou"></i>
+            <i class="icon iconfont icon-bofang" v-show="!isPlay"></i>
+            <i class="icon iconfont icon-zanting" v-show="isPlay"></i>
+            <i class="icon iconfont icon-xiayishou"></i>
+          </div>
+          <span @click="toggleList=!toggleList" class="album-info-control-menu">歌单</span>
+        </div>
+      </div>
     </div>
+    <!-- 歌单 -->
+    <transition name="slide">
+      <ul class="music-list" v-show="toggleList">
+      <li
+        @click="selected(music,index)"
+        v-for="(music,index) in musicList"
+        :key="index"
+        :class="['music-list-item', nowIndex==index ? 'selected' : '']"
+      >
+        <span class="music-list-item-title">{{music.title}}&nbsp;-&nbsp;</span>
+        <span class="music-list-item-author">{{music.author}}</span>
+      </li>
+    </ul>
+    </transition>
+    <!-- 播放控件 -->
+    <audio :src="musicSrc" autoplay controls class="audio"></audio>
+  
+  </div>
 </template>
 <script>
+import "@/assets/font/iconfont.css";
 export default {
-    
-}
+  props: ["musicList"],
+  data() {
+    return {
+      nowIndex: -1, // 当前选中歌曲的索引
+      albumImg:
+        "http://img3.imgtn.bdimg.com/it/u=1039246244,1205520727&fm=27&gp=0.jpg", //专辑的封面
+      albumTitle: "", //歌曲名称
+      albumAuthor: "", //歌曲歌手
+      isPlay: false, // 是否在播放
+      toggleList: true, //歌单的是否显示
+      musicSrc: "", // 歌曲的URL
+      lrcList: [], // 歌词的数组
+      lrcIndex: -1
+    };
+  },
+  methods: {
+    selected(music, index) {
+      this.nowIndex = index;
+      this.albumImg = music.musicImgSrc;
+      this.albumTitle = music.title;
+      this.albumAuthor = music.author;
+      this.musicSrc=music.src;
+    }
+  }
+};
 </script>
-<style >
+<style lang="scss" scoped>
+.clearfix::after {
+  content: "";
+  display: block;
+  clear: both;
+}
+.music-list {
+  position: fixed;
+  bottom: 2rem;
+  width: 100%;
+  background-color: #2a2929;
+  height: 4rem;
+  overflow-y: scroll;
+  &-item {
+    color: #dcdbdb;
+    border-bottom: 0.02rem solid #343433;
+    padding: 0.2rem;
 
+    &.selected {
+      color: #299557;
+    }
+  }
+}
+
+.album {
+  height: 2.3rem;
+  display: flex;
+  text-align: center;
+  position: relative;
+  color: #fff;
+  &-mask {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    filter: blur(20px);
+    z-index: -1;
+  }
+
+  &-img {
+    flex-grow: 1;
+    width: 0;
+    margin-left: 0.2rem;
+    img {
+      width: 100%;
+    }
+  }
+  &-info {
+    width: 0;
+    flex-grow: 2;
+    &-title {
+      font-size: 0.5rem;
+    }
+    &-control {
+      position: relative;
+      height: 0.9rem;
+      line-height: 0.9rem;
+      &-icon {
+        float: left;
+        position: absolute;
+        left: 50%;
+        transform: translateX(-50%);
+        .icon {
+          font-size: 0.5rem;
+        }
+      }
+      &-menu {
+        float: right;
+        margin-right: 0.4rem;
+      }
+    }
+  }
+}
+
+.slide {
+  &-enter {
+    transform: translateY(100%);
+    &-to {
+      transform: translateY(0);
+    }
+    &-active {
+      transition: transform 1s ease;
+    }
+  }
+
+  &-leave {
+    transform: translateY(0);
+    &-to {
+      transform: translateY(100%);
+    }
+    &-active {
+      transition: transform 1s ease;
+    }
+  }
+}
+
+.audio {
+  background: #fff;
+  height: 1rem;
+  position: fixed;
+  bottom: 1rem;
+  width: 100%;
+  &-ctrl {
+    width: 100%;
+  }
+}
+
+.lrclist {
+  text-align: center;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 2rem;
+  top: 3.3rem;
+  overflow-y: scroll;
+  z-index: -1;
+  padding-top: 2rem;
+
+  .selected {
+    color: #299557;
+    font-size: 120%;
+  }
+}
 </style>
--- src/views/music/Music.vue
+++ src/views/music/Music.vue
@@ -1,11 +1,21 @@
 <template> 
-        <player></player>
+        <player :musicList="musicList"></player>
 </template>
 <script>
 import Player from '@/components/Player.vue';
+import axios from 'axios'
 export default {
+    data(){
+       return{
+            musicList:[],
+       }
+    },
     created () {
-        this.$emit("switchTab","music")
+        this.$emit("switchTab","music");
+        axios.get('/data/musicdata.json').then(res=>{
+            console.log(res)
+            this.musicList=res.data.musicData
+        })
     },
     components: {
         Player
